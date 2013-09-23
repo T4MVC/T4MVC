246553ef4a98e4e2e224e5f4dc5523cae23931cf
Kevin Kuszyk <kevin@kevinkuszyk.com>
2013-09-23 15:26:10 +0100

    Added a sample bundle and controller to test bundling is working.

---
+++ T4MVCHostMvcApp/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Web.Optimization;
+
+namespace T4MVCHostMvcApp.App_Start
+{
+    public static class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        [SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle(Links.Bundles.Content.Styles).Include(
+                Links.Bundles.Content.Assets.Site_css
+                ));
+
+        }
+    }
+}
+
+namespace Links
+{
+    public static partial class Bundles
+    {
+        public static partial class Content
+        {
+            public const string Styles = "~/content/styles";
+        }
+    }
+}
