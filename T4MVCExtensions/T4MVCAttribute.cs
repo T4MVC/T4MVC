99e06df6c84d9e0e4beeb725a0c80368946b9ddf
Miguel Ignacio <a@b.com>
2015-01-29 17:18:15 +0200

    added [T4MVC] attribute for controllers removed obsolete "ControllerNamePattern" from T4MVC.tt.settings.xml

---
+++ T4MVCExtensions/T4MVCAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace System.Web.Mvc {
+
+  [AttributeUsage(AttributeTargets.Class)]
+  public class T4MVCAttribute : Attribute {
+
+
+    public T4MVCAttribute() : this(true) {
+    }
+
+    public T4MVCAttribute(bool process) {
+      Process = process;
+    }
+
+
+    public bool Process { get; private set; }
+
+
+  }
+}
