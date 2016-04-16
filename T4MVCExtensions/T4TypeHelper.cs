352b241452c6c25a7577e20caf4b5c024b446e87
Matthew Steeples <matthew@mercuryit.co.uk>
2016-04-17 00:43:41 +0100

    Add helper method to determine if a type was generated by T4MVC

---
+++ T4MVCExtensions/T4TypeHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace T4MVCExtensions
+{
+    public static class T4TypeHelper
+    {
+        public static bool IsT4MVC(this Type t)
+        {
+            if (t.FullName.StartsWith("T4MVC"))
+                return true;
+
+            var attribute = t.GetCustomAttributes(typeof(GeneratedCodeAttribute), false)
+                .OfType<GeneratedCodeAttribute>().FirstOrDefault();
+
+            if (attribute != null)
+            {
+                if (attribute.Tool == "T4MVC")
+                    return true;
+            }
+
+            if (t.IsNested)
+            {
+                var parent = t.DeclaringType;
+                if (parent != null)
+                    return parent.IsT4MVC();
+            }
+
+            return false;
+        }
+    }
+}
