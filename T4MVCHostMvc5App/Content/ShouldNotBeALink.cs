6865437d8b6dfda5704680d215bc62ae8d7e7706
KrzysztofZ <k.zmorzynski@gmail.com>
2016-12-11 15:29:13 +0100

    Removed unncessary "new" MVC5 host website and tests
    Since old MVC3 website was upgraded to MVC5 there's no need to keep this
    test MVC5 hostwebsite (and tests).

--- T4MVCHostMvc5App/Content/ShouldNotBeALink.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace T4MVCHostMvc5App.Content {
-    public class IgnoreMe {
-    }
-}
--- T4MVCHostMvc5App/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="T4MVCHostMvc5App.MvcApplication" Language="C#" %>
